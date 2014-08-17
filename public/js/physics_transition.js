44baa64b47b7dad80448caff0cd49fad849eea1d
Ariel Fogel <fogeltine@gmail.com>
2014-08-17 00:49:34 -0700

    Implement MVC for Physics Transition Module

--- public/js/physics_transition.js
+++ public/js/physics_transition.js
@@ -11,8 +11,9 @@ Module.PhysicsTransition = function() {
       console.log("starting sample app");
       //cleans up the main main-container
       $("#main-container").empty();
-      //Enter Your View Render here
-      Module.PhysicsTransition.View.render();
+      //Enter Your Module Controller here
+      new Module.PhysicsTransition.Controller(Module.PhysicsTransition.View
+                                              ,Module.PhysicsTransition.Model);
 
       setTimeout(function() {
         //Your module can only be 30 seconds long,
@@ -30,18 +31,8 @@ Module.PhysicsTransition = function() {
 //Use the start function to display your view.
 //DONT put your view call in the setTimeout.
 
-Module.PhysicsTransition.View = {
-  render: function() {
-    this.removeBackground()
-    this.addBackgroundColor()
-  },
-
-  removeBackground: function(){
-    // remove the background image if exists
-    $(document.body).css("background-image", "none")
-  },
-  addBackgroundColor: function() {
-    var colorArray = ['#95D3E2'
+Module.PhysicsTransition.Model = function(){
+  this.colorArray =  ['#95D3E2'
                       ,'#254D78'
                       ,'#825D75'
                       ,'#F23E32'
@@ -51,13 +42,72 @@ Module.PhysicsTransition.View = {
                       ,'#FEDB74'
                       ,'#4D4550'
                       ,'#E7E3E3'];
-    var randomColor = colorArray[Math.floor((Math.random() * 10))];
-    // add background color
-    $(document.body).css("background-color", randomColor)
+  this.transitionArray = ['js/ball_roll.js']
+}
+
+Module.PhysicsTransition.Model.prototype = {
+  produceRandomColor: function(){
+    var colorArrayLength = this.colorArray.length;
+    var randomIndex = Math.floor((Math.random() * colorArrayLength));
+    return this.colorArray[randomIndex];
+  },
+
+  produceRandomTransition: function(){
+    var transitionArrayLength = this.transitionArray.length;
+    var randomIndex = Math.floor((Math.random() * transitionArrayLength));
+    return this.transitionArray[randomIndex];
   }
+}
+// -------------------------------------------------------------- //
+// -------------------------------------------------------------- //
+Module.PhysicsTransition.Controller = function(view, model) {
+  this.view  = new view;
+  this.model = new model;
+  this.init();
+}
 
-    // add canvas tag and physics library
-};
+Module.PhysicsTransition.Controller.prototype = {
+  init: function(){
+    var color = this.model.produceRandomColor();
+    var transition = this.model.produceRandomTransition();
+    this.view.cleanSlate();
+    this.view.render(color, transition);
+  }
+}
 
+// ------------------------------------------------- //
+// ------------------------------------------------- //
 
+Module.PhysicsTransition.View = function() {}
 
+Module.PhysicsTransition.View.prototype = {
+  render: function(color, transition){
+    this.addBackgroundColor(color);
+    this.addCanvas();
+    this.addTransition(transition);
+  },
+
+  cleanSlate: function(){
+    this.removeBackground();
+  },
+
+  removeBackground: function(){
+    // remove the background image if exists
+    $(document.body).css("background-image", "none")
+  },
+
+  addBackgroundColor: function(color) {
+    // add background color
+    $(document.body).css("background-color", color)
+  },
+
+  addCanvas: function(){
+    var canvas = $('<canvas>').addClass('viewpoint')
+    $('#main-container').append(canvas)
+  },
+
+  addTransition: function(transition){
+    var source = $('<script>').attr('src', transition)
+    $('body').append(source)
+  }
+}
