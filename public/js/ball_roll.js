84a323d7565d03587d825e0e4ed289004abc6d7c
Ariel Fogel <fogeltine@gmail.com>
2014-08-17 17:13:31 -0700

    Add logic to animate ball's motion across the screen.

--- public/js/ball_roll.js
+++ public/js/ball_roll.js
@@ -1,21 +1,38 @@
 Physics(function( world ){
   var canvasHeight = (window.innerHeight/10)*9
   var canvasWidth = (window.innerWidth/10)*9
+
+  // Create a renderer
   var renderer = Physics.renderer('canvas', {
     el: 'viewport',
     width: canvasWidth,
     height: canvasHeight
   });
 
+  // add renderer to the world
   world.add( renderer );
-
-
-  var circle = Physics.body('circle', {
-    x: 140,
-    y: (canvasHeight/4),
-    vx: .75,
-    radius: 50
+  // render on each step
+  world.on('step', function(){
+    world.render();
   });
+
+  // Create Main Ball
+  var circle = Physics.body('circle', {
+    x: 140
+    ,y: (canvasHeight/4)
+    ,radius: 50
+    ,vx: .01 //speed of the ball's movement
+  });
+
   world.add( circle );
   world.render();
+
+  // subscribe to ticker to advance the simulation
+  Physics.util.ticker.on(function( time, dt ){
+    world.step( time );
+  });
+
+  // start the ticker
+  Physics.util.ticker.start();
+
 })
