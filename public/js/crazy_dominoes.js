399dc7688bee0d4b8df54e276c12d73fb4a2840a
Ariel Fogel <fogeltine@gmail.com>
2014-08-17 20:40:34 -0700

    Add dominoes to world, render to screen.

--- public/js/crazy_dominoes.js
+++ public/js/crazy_dominoes.js
@@ -14,4 +14,27 @@ Physics(function( world ){
   });
 
   world.add( renderer );
+
+  var dominoes = [];
+    for ( var i = 0, l = 35; i < l; ++i ){
+
+      dominoes.push( Physics.body('rectangle', {
+        width: 10
+        ,height: 70
+        ,x: (i*25) + (viewWidth/3)
+        ,y: viewHeight - 40
+        ,vx: 0
+        ,mass: 5
+        ,cof: 0
+        ,restitution: 3
+        ,styles: {
+            fillStyle: '#000000'
+            ,angleIndicator: '#FFFFFF'
+        }
+      }));
+    }
+
+  world.add( dominoes );
+  world.render()
+
 })
