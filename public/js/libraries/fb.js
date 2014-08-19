1d1b01865dbf9a46dbc880c60850e72be6102839
Kendall <kendall.carey@gmail.com>
2014-08-19 15:18:41 -0700

    new updated landing page

--- public/js/libraries/fb.js
+++ public/js/libraries/fb.js
@@ -79,8 +79,8 @@
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '! <br> Click Start below!';
+      document.getElementById('start').innerHTML =
+        'Thanks for logging in, ' + response.name + '! <br> Click to get the time!';
         fbuser.profile = response;
     });
   }
