1377d14ed5f88087dbdbc06e8cd42c1d918f0541
Ariel Fogel <fogeltine@gmail.com>
2014-08-19 10:26:44 -0700

    Remove commented code from old master controller that is now refactored.

--- public/js/mastercontroller.js
+++ public/js/mastercontroller.js
@@ -1,18 +1,7 @@
 $(document).ready(function() {
-  // newPicture = new Module.Picture();
-  // newHelloUser = new Module.HelloUser();
-  // newTagCanvas = new Module.TagCanvas();
-  // newWemo = new Module.Wemo();
-  // moduleList = [newHelloUser,
-  //               new Module.PhysicsTransition(),
-  //               newPicture,
-  //               new Module.PhysicsTransition(),
-  //               newTagCanvas];
-  // moduleList = [newTagCanvas]
   var currentUserInfo;
   master = new MainController(MainModel, MainView);
   new MasterBinder({start: '#start'}, master).bindToSelectors();
-  // master.bindListeners();
   // MusicController();
 });
 
@@ -138,48 +127,6 @@ MainView.prototype = {
   }
 }
 
-// function MainController(moduleList) {
-//   this.moduleList = moduleList;
-//   this.moduleIndex = 0;
-//   this.loadCount = 0;
-//   var self = this;
-
-//   this.init = function() {
-//     currentUserInfo = new CurrentUserInfo();
-//     console.log(currentUserInfo);
-//     $(this.moduleList).trigger('load');
-//   };
-
-//   this.bindListeners = function() {
-//     $(document).on('loaded',function() {
-//       self.loadCount++;
-//       if (self.loadCount === self.moduleList.length) {
-//         $(document).trigger('next');
-//       }
-//     });
-
-//     $('#start').on('click', this.init.bind(master));
-
-//     $(document).on('next',function() {
-//       $(this.moduleList[this.moduleIndex]).trigger('start');
-//       this.moduleIndex++;
-//       new BackgroundColorPicker.Controller(BackgroundColorPicker.View,BackgroundColorPicker.Model)
-//     }.bind(this));
-//   };
-// }
-
-// function CurrentUserInfo() {
-//   var formInput = $("#form input").serializeArray();
-//   this.name = formInput[0].value;
-//   this.love = formInput[1].value;
-//   this.phone = formInput[2].value;
-//   this.likes = fbuser.likes;
-//   this.fbprofile = fbuser.profile;
-//   this.fbphotos = fbuser.photos;
-//   this.fbposts = fbuser.posts;
-//   this.fbplaces = fbuser.places;
-// }
-
 // function MusicController(){
 // 	$('#audio')[0].volume = 0;
 // 	$('#volume').click(toggleVolumeIcon);
@@ -205,56 +152,4 @@ MainView.prototype = {
 // 		$audio.volume = 0;
 // 	}
 // }
-
-// BackgroundColorPicker = {}
-
-// BackgroundColorPicker.Model = function(){
-//   this.colorArray =  ['#95D3E2'
-//                       ,'#254D78'
-//                       ,'#825D75'
-//                       ,'#F23E32'
-//                       ,'#FAB562'
-//                       ,'#D2E594'
-//                       ,'#476054'
-//                       ,'#FEDB74'
-//                       ,'#4D4550'
-//                       ,'#E7E3E3'];
-// }
-
-// BackgroundColorPicker.Model.prototype = {
-//   produceRandomColor: function(){
-//     var colorArrayLength = this.colorArray.length;
-//     var randomIndex = Math.floor((Math.random() * colorArrayLength));
-//     return this.colorArray[randomIndex];
-//   },
-// }
-
-// BackgroundColorPicker.Controller = function(view, model) {
-//   this.view  = new view;
-//   this.model = new model;
-//   this.init();
-// }
-
-// BackgroundColorPicker.Controller.prototype = {
-//   init: function(){
-//     var color = this.model.produceRandomColor();
-//     this.view.removeBackground;
-//     this.view.render(color);
-//   }
-// }
-
-// BackgroundColorPicker.View = function() {}
-
-// BackgroundColorPicker.View.prototype = {
-//   render: function(color, transition){
-//     this.addBackgroundColor(color);
-//   },
-
-//   addBackgroundColor: function(color) {
-//     // add background color
-//     $(document.body).css("background-color", color)
-//   },
-// }
-
-
 
