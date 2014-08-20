d0dec77ca1267a7f5e455d17bc67ec988ec1b8d1
Apprentice <apprentice@master-inf-5.local>
2014-08-19 18:49:41 -0700

    google map is dropping icons now

--- public/js/modules/places.js
+++ public/js/modules/places.js
@@ -3,25 +3,18 @@ Module.Places = function() {
   this.init = function() {
 
     $(this).on('load', function() {
-      console.log("loading Places app");
       //preload some stuff to get your module ready
       $(document).trigger('loaded');
     });
 
     $(this).on('start', function() {
-      console.log("starting Places app");
       //cleans up the main main-container
       $("#main-container").empty();
-      //Enter Your View Render here
       new Module.Places.Controller(Module.Places.View, Module.Places.Model);
-      // Module.View.render();
 
       setTimeout(function() {
-        //Your module can only be 30 seconds long,
-        //you can remove the timeout if the animation is less than 30 seconds
-        console.log("Places app ending");
         $(document).trigger('next');
-      },30000);
+      },15000);
     });
   };
   //initialize this Module
@@ -51,42 +44,53 @@ Module.Places.Controller = function(view, model) {
 
 Module.Places.Controller.prototype = {
   init: function(){
-    $('#main-container').css("height","1200px");
+    var markers = [];
+    var iterator = 0;
     var locations = [];
+    var bounds = new google.maps.LatLngBounds();
+
+    $('#main-container').append('<div id="title"><center>'+
+                                '<h1 style="margin:0px;">'+
+                                'Places you\'ve been</h1>'+
+                                '</center></div>');
+    $('#main-container').append('<div id="map"></div>');
+    $('#map').css("height","800px");
+    $('#title').css("height","200px");
+
     //create array of location
     places_list = currentUserInfo.fbplaces.data;
-
     places_list.forEach(function(location){
-      locations.push([location.place.name, location.place.location.latitude, location.place.location.longitude])
+      locLatLng = new google.maps.LatLng(location.place.location.latitude, location.place.location.longitude)
+      bounds.extend(locLatLng);
+      locations.push(locLatLng);
+
     })
 
-    var map = new google.maps.Map(document.getElementById('main-container'), {
-      zoom: 11,
-      center: new google.maps.LatLng(37.7833, -122.4167),
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      //Set default center location to san francisco.  It will auto adjust to your tags.
+      center: new google.maps.LatLng(20.7833, 122.4167),
+
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
-    var infowindow = new google.maps.InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
-
     for (var i = 0; i < locations.length; i++) {
-      var pos = new google.maps.LatLng(locations[i][1], locations[i][2]);
-        bounds.extend(pos);
-        marker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            // animation: google.maps.Animation.DROP,
-            icon: currentUserInfo.fbprofilepic.data.url
-          });
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(this.marker, i));
+      setTimeout(function(){
+        dropMarker();
+      }, i * 300)
+      // marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+    map.fitBounds(bounds);
 
-      map.fitBounds(bounds);
+    function dropMarker(){
+      markers.push(new google.maps.Marker({
+      position: locations[iterator],
+      map: map,
+      draggable: false,
+      icon: currentUserInfo.fbprofilepic.data.url,
+      animation: google.maps.Animation.DROP
+    }));
+    iterator++;
     }
   }
 };
